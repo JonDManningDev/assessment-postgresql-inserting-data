c610aba17af5aaa9a4a4f642b0f9df2d0545d862
Jonathan <jondmanning1986@gmail.com>
2025-02-19 16:37:47 -0500

    Initial commit

---
+++ src/artists/insert-multiple-artists.sql
@@ -0,0 +1,4 @@
+INSERT INTO artists (first_name, last_name, birthday, is_alive) VALUES
+('Pablo', 'Picasso', '1881-10-25', FALSE),
+('Frida', 'Kahlo', '1907-07-06', FALSE),
+('Jackson', 'Pollock', '1912-01-28', FALSE);
